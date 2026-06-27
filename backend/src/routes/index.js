671377d4d142aa02a45609230a203decd433dd2a
Sarjak Bhandari <bhandarisarjak@gmail.com>
2026-06-27 23:13:28 +0545

    mfa routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const healthRoutes = require('./health.routes');
 const authRoutes = require('./auth.routes');
+const mfaRoutes = require('./mfa.routes');
 
 const router = express.Router();
 
 router.use('/health', healthRoutes);
 router.use('/auth', authRoutes);
+router.use('/mfa', mfaRoutes);
 
 module.exports = router;
